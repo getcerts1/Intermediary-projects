8843f85f1974bc7ec6e4f1f95d0c3230c3c8581e
getcerts1 <barrowtech1999@gmail.com>
2024-12-20 15:19:48 +0300

    new approach

---
+++ .history/arraysort_20241220150902.py
@@ -0,0 +1,12 @@
+def sorting(array):
+    i = 1
+    list_a = []
+    while i in array:
+        if i not in list_a:
+            list_a.append(i)
+            array.remove(i)
+        i += 1
+    return list_a
+
+newarray = [7, 4, 6, 7, 5, 3, 2, 1]
+print(sorting(newarray))
